5c6f9cec6af4fec8b95601311823013a37b051d5
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-12-01 10:47:30 +0100

    feat: Añadir ejercicio Stop gninniS My sdroW!.py

---
+++ ejercicios/Stop gninnipS My sdroW!.py
@@ -0,0 +1,16 @@
+def spin_words (sentence):
+
+    return ' '.join(word[::-1] if len(word) >= 5 else word for word in sentence.split(' ')).lstrip()
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    assert spin_words("Welcome") == "emocleW"
+    assert spin_words("to") == "to"
+    assert spin_words("CodeWars") == "sraWedoC"
+    assert spin_words("Hey fellow warriors") == "Hey wollef sroirraw"
+    assert spin_words("This sentence is a sentence") == "This ecnetnes is a ecnetnes"
